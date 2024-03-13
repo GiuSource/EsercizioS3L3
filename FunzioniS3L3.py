6fdbc38cf5ef55fa2344b703e990c81863315289
giuSource <giulio.sorgente@yahoo.it>
2024-03-13 16:41:13 +0100

    Up

---
+++ FunzioniS3L3.py
@@ -0,0 +1,13 @@
+def area_quadrato(lato):
+    f = lato * 4
+    return f
+
+def area_cerchio(raggio):
+    f = 2 * 3.14 * raggio
+    return f
+
+def area_rettangolo(base, altezza):
+    f = (base * 2) + (altezza * 2)
+    return f
+
+
